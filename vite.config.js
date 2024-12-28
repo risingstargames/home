cf42fec200a6acfe9bf7d4b4387ae98dfa52e14e
risingstargames <risingstargames.email@gmail.com>
2024-12-28 06:07:01 +0530

    jjj

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/risingstargames.github.io",
+  base: "/risingstargames.github.io/",
   plugins: [react()],
 })
